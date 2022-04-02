3019d04152b476cccbd970301871c330be251fc7
Broderick Poke <blpoke@utas.edu.au>
2022-04-02 14:50:26 +1100

    Initial Commit

---
+++ BetterOOinCSharp/BetterOOinCSharp/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterOOinCSharp
+{
+    public enum Gender { M, F, X };
+    
+    /*
+     * Task 2.1.1
+     */
+    public class Employee
+    {
+        //These properties have auto-generated getters (accessors <- fancy name) and
+        //setters (mutators <- again fancier name); hence, are entirely public
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public Gender Gender { get; set; }
+
+        public override string ToString()
+        {
+            return Name + '\t' + ID + '\t' + Gender;
+        }
+
+    }
+}
---
+++ BetterOOinCSharp/BetterOOinCSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterOOinCSharp
+{
+    class Program
+    {
+        // Task 2.2.1
+        public delegate Employee ManageWorker(int id);
+        static void Main(string[] args)
+        {
+            // Task 2.1.4 & 2.1.5
+            Boss boss1 = new Boss();
+
+            boss1.Display();
+            Console.WriteLine("Working with {0}", boss1.Use(1));
+            Console.WriteLine("After working with that employee");
+            boss1.Display();
+
+            //Task 2.2.2
+            Action doSomething;
+            ManageWorker manage;
+            Boss boss2 = new Boss();
+
+            doSomething = boss2.Display;
+            manage = boss2.Use;
+            // manage = boss2.Fire;   Task 2.2.3
+
+            doSomething();
+            Console.WriteLine("Dealing with {0}", manage(1));
+            Console.WriteLine("After dealing with that employee");
+            doSomething();
+
+        }
+    }
+}
