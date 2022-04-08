47d5922f98748a5b09349e6701a40af0cb3e78aa
brod poke <blpoke22@gmail.com>
2022-04-08 21:33:50 +1000

    added training session class

--- BetterOOinCSharp/BetterOOinCSharp/Agency.cs
+++ BetterOOinCSharp/BetterOOinCSharp/Agency.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace BetterOOinCSharp
 {
-
     /*
      * Task 2.1.2
      */
---
+++ BetterOOinCSharp/BetterOOinCSharp/TrainingSession.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterOOinCSharp
+{
+    class TrainingSession
+    {
+        public string title;
+        public int year;
+        public DateTime certified;
+        public string mode;
+
+        public override string ToString()
+        {
+            return title + '\t' + year '\t' + certified + '\t' + mode;
+        }
+    }
+}
